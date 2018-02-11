45d487dc2d55c79b1ec5325d648a9d9d5febba2f
Geoff Whitehead <geoff1012@gmail.com>
2018-02-11 15:04:47 +0000

    basic setup done

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+
+// @flow
+import React from "react";
+import { render } from "react-dom";
+const App = () => <h1>Setting up React with Parcel bundler</h1>;
+const wrapper = document.getElementById("app");
+wrapper ? render(<App />, wrapper) : false;
