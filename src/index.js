9f85ad8b2fd745d59932c40aec9eb777f6c6c11e
Geoff Whitehead <geoff1012@gmail.com>
2018-02-11 17:13:59 +0000

    refactored

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 // @flow
 import React from "react";
 import { render } from "react-dom";
-const App = () => <h1>Setting up React with Parcel bundler</h1>;
+import Main from './demo/App'
+
+const App = () => <Main/>;
 const wrapper = document.getElementById("app");
 wrapper ? render(<App />, wrapper) : false;
