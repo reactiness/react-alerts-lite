413aa7c5e4cb4988c7ec9a09d2c74cac36a2e616
Geoff Whitehead <geoff1012@gmail.com>
2018-03-21 20:57:23 +0000

    test environment setup but jest isnt able to find test files

--- tools/enzymeTestAdapterSetup.js
+++ tools/enzymeTestAdapterSetup.js
@@ -1,6 +1,6 @@
 import { configure, shallow, render, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import "raf/polyfill";
+// import "raf/polyfill";
 
 // Make Enzyme functions available in all test files without importing
 global.shallow = shallow;
