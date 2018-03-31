dcab187f22d4407a09fc9fb2059f1d2f52fd2ba3
Geoff Whitehead <geoff1012@gmail.com>
2018-03-31 14:43:08 +0100

    linted

--- stories/transitionsStory.js
+++ stories/transitionsStory.js
@@ -5,9 +5,9 @@ import { Alerts } from "../src/core";
 import { AlertProvider } from "../src/core/AlertProvider";
 
 const content =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
 
-  storiesOf("Transitions", module)
+storiesOf("Transitions", module)
   .add("No Transition", () => (
     <div>
       <AlertProvider theme="flat" />
