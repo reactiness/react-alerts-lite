7c084c1176a2dfde80d8111e92f50920225e9137
Geoff Whitehead <geoff1012@gmail.com>
2018-03-31 16:13:18 +0100

    tests now handling assets

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  moduleNameMapper: {
+    "\\.(svg|css|scss)$": "<rootDir>/tools/assetsTransformer.js"
+  },
   setupFiles: ["./tools/jestSetup.js"],
   snapshotSerializers: ["enzyme-to-json/serializer"]
 };
