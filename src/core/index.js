355831694ee83c95e7aad0ef645c46a309371128
Geoff Whitehead <geoff1012@gmail.com>
2018-04-01 14:30:00 +0100

    removed demo.

--- src/core/index.js
+++ src/core/index.js
@@ -33,3 +33,5 @@ export class Alerts {
     remove(type);
   }
 }
+
+export default Alerts;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import React from "react";
-import { render } from "react-dom";
-import Main from "./demo/App";
-
-const App = () => <Main />;
-const wrapper = document.getElementById("app");
-wrapper ? render(<App />, wrapper) : false;
+export { Alerts } from "./core";
+export { AlertProvider } from "./core/AlertProvider";
